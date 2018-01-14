f0b4297bfc7931aba42ebbd76bc0684e3e386467
Superman <supermanchc@gmail.com>
2018-01-14 16:21:52 +0800

    Update fa icon scss

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default () => (
   <div id="footer">
-    <a href="/." rel="nofollow">
+    <a href="/" rel="nofollow">
       Superman‘s Blog.
     </a>
     <a rel=" noopener noreferrer" target="_blank" href="https://github.com/tufu9441/maupassant-hexo">
@@ -12,10 +12,10 @@ export default () => (
     <a rel="nofollow" target="_blank" href="https://github.com/pagecho">
     &nbsp;Cho.
     </a>
-    <a style={{ display: 'block' }}>
+    {/* <a style={{ display: 'block' }}>
       <span>本站共 </span>
       <span id="site-view">29876</span>
       <span>次访问</span>
-    </a>
+    </a> */}
   </div>
 )
