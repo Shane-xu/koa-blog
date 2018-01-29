97d66fc65e533a7d8a626b847d7a48951a6c112e
Superman <supermanchc@gmail.com>
2018-01-29 21:41:44 +0800

    refactor(Index Route): Change Index Route

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,28 +8,33 @@ class Header extends Component {
       navs: [
         {
           name: '首页',
-          link: '/posts',
-          icon: 'home'
+          link: '/',
+          icon: 'home',
         },
         {
           name: '归档',
           link: '/archives',
-          icon: 'archive'
+          icon: 'archive',
         },
         {
           name: '关于',
           link: '/about',
-          icon: 'user'
-        }
-      ]
+          icon: 'user',
+        },
+      ],
     }
   }
   renderNav() {
     const { navs } = this.state
-    return navs.map((nav) => {
+    return navs.map(nav => {
       const iconClass = `fa fa-${nav.icon}`
       return (
-        <Link to={nav.link} key={nav.name} activeClassName="current">
+        <Link
+          to={nav.link}
+          key={nav.name}
+          activeClassName="current"
+          onlyActiveOnIndex
+        >
           <i className={iconClass}>{nav.name}</i>
         </Link>
       )
@@ -41,11 +46,11 @@ class Header extends Component {
       <div id="header">
         <div className="site-name">
           <hi className="hidden">Superman Blog</hi>
-          <a href="./" id="logo">Superman Blog</a>
+          <a href="./" id="logo">
+            Superman Blog
+          </a>
           {/* <p className="description"></p> */}
-          <div id="nav-menu">
-            {this.renderNav()}
-          </div>
+          <div id="nav-menu">{this.renderNav()}</div>
         </div>
       </div>
     )
@@ -53,4 +58,3 @@ class Header extends Component {
 }
 
 export default Header
-
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,30 +1,37 @@
 export default {
-  childRoutes: [{
-    path: '/',
-    component: require('../containers/index').default,
-    indexRoute: {
-      onEnter: (nextState, replace) => replace('/posts')
+  childRoutes: [
+    {
+      path: '/',
+      component: require('../containers/index').default,
+      indexRoute: {
+        component: require('../containers/Post').PostListPage,
+      },
+      childRoutes: [
+        {
+          path: 'posts',
+          getComponent(nextState, cb) {
+            cb(null, require('../containers/Post').PostListPage)
+          },
+        },
+        {
+          path: 'post/:id',
+          getComponent(nextState, cb) {
+            cb(null, require('../containers/Post').PostPage)
+          },
+        },
+        {
+          path: 'archives',
+          getComponent(nextState, cb) {
+            cb(null, require('../containers/Archive').ArchivePage)
+          },
+        },
+        {
+          path: 'about',
+          getComponent(nextState, cb) {
+            cb(null, require('../containers/About').AboutPage)
+          },
+        },
+      ],
     },
-    childRoutes: [{
-      path: 'posts',
-      getComponent(nextState, cb) {
-        cb(null, require('../containers/Post').PostListPage)
-      }
-    }, {
-      path: 'post/:id',
-      getComponent(nextState, cb) {
-        cb(null, require('../containers/Post').PostPage)
-      }
-    }, {
-      path: 'archives',
-      getComponent(nextState, cb) {
-        cb(null, require('../containers/Archive').ArchivePage)
-      }
-    }, {
-      path: 'about',
-      getComponent(nextState, cb) {
-        cb(null, require('../containers/About').AboutPage)
-      }
-    }]
-  }]
+  ],
 }
